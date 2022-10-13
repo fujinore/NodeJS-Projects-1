ab26f7beff4eb43907d1c530d29043a8ffd9ce0b
Fujin Ore <84015052+fujinore@users.noreply.github.com>
2022-10-14 00:59:10 +0700

    Update fujinore.js

--- fujinore.js
+++ fujinore.js
@@ -1,13 +1,26 @@
-const data = [
-  { id: 1, name: "john" },
-  { id: 2, name: "doe" },
-];
+var express = require("express");
+var app = express();
+var mysql = require("mysql");
+var bodyParser = require("body-parser");
+var cors = require("cors");
 
-(() => {
-  console.log(
-    data.reduce((obj, item) => {
-      obj[item.id] = item;
-      return obj;
-    }, {})
-  );
-})();
+app.use(bodyParser.json({ type: "application/json" }));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cors());
+
+var con = mysql.createConnection({
+  host: "koempro-server.cwsly2cix1oo.us-east-1.rds.amazonaws.com",
+  user: "admin",
+  password: "adminintek",
+  database: "koempro_server",
+});
+
+con.connect((error) => {
+  if (error) console.log(`${error} Ada yang salah!`);
+  else console.log("connected");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello Koem!");
+});
